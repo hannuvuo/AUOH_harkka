7eff395171151b4158fd774fbc9cd7b70c2dfd6f
hannuvuo <hannu.vuorenmaa@seamk.fi>
2020-05-10 13:34:01 +0300

    0.5

--- machining_model.js
+++ machining_model.js
@@ -1,17 +1,13 @@
 const mongoose = require('mongoose');
 const Schema = mongoose.Schema;
 
-const schema = new Schema({
-    id: {
-        type: Number,
+const schema = new Schema({    
+    name: {
+        type: String,
         required: true,
         index: {
             unique: true
-        }
-    },    
-    name: {
-        type: String,
-        required: true        
+        }        
     },
     material: {
         type: String,
